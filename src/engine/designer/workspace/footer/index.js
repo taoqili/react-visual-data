52e34a4e4f38a210eb2af081e587c163db909e8a
taoqili <taoqili@kuaishou.com>
2021-12-21 18:42:18 +0800

    feat: aside 和footer 复用dock

--- src/engine/designer/workspace/footer/index.js
+++ src/engine/designer/workspace/footer/index.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import { Button, Space, Tooltip } from "antd";
-import { useView } from "../../../hooks/useDesigner";
-import { IconFont } from "~components";
-import { round } from "~utils/helper";
 import Dock from '../Dock'
 import docks from './docks'
 import './index.less'
 
 export default () => {
-  const { view, setView } = useView();
-  const { scale } = view;
-
-  const handleSetting = () => {
-    setView({
-      isShowReferLine: false,
-      lines: {
-        h: [],
-        v: []
-      }
-    });
-  };
-
   return (
     <div className={"lcp-design-footer"}>
       <Dock docks={docks} horizontal />
